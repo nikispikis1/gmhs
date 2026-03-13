d39846378826bd89c351cfcd6663cde131df2f70
nikispikis1 <nikoldvorakova2502@icloud.com>
2026-03-13 09:58:24 +0100

    .

---
+++ 4. lekce/script.js
@@ -0,0 +1,47 @@
+let userName = "Nikol";
+console.log(userName)
+
+userName = "Honzík";
+console.log(userName)
+
+console.log(7+4)
+console("7"+"4")
+console("A"+"B")
+console("7"+4)
+
+let age = 31;
+console.log(age > 20)
+
+let text = true;
+console.log(typeof text)
+    
+
+let hodnota = null;
+console,log(hodnota)
+
+let cisloA = 10+
+let cisloB = 5;
+console.log(cisloA + cisloB)
+console.log(cisloA - cisloB)
+console.log(cisloA * cisloB)
+console.log(cisloA / cisloB)
+
+// Umocňování **
+console.log(cisloA ** cisloB)
+// Zbytek
+
+console.log(cisloA % cisloB)
+
+console.log(cisloA + cisloB * cisloB)
+
+console.log((cisloA + cisloB) *cisloB)
+//aktualizace proiměnné
+
+let number = 18;
+number = number + 1;
+number += 1;
+number -= 2;
+number *= 4
+number /= 6;
+number %= 3;
+console.log(number);
