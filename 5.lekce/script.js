095a7bec1c37631b4e4c743ff9960fc1e58a3cd5
nikispikis1 <nikoldvorakova2502@icloud.com>
2026-03-20 09:56:25 +0100

    .

---
+++ 5.lekce/script.js
@@ -0,0 +1,6 @@
+let age = 21;
+if (age >20) {
+    console.log("You are over 20.");
+} else {
+    console.log("You are 20 or younger.");
+}
---
+++ 6.lekce/script.js
@@ -0,0 +1,22 @@
+// Vnořené podmínky
+let isloggedIn = true;
+let isAdmin = false;
+
+if(isloggedIn) {
+console.log("vítejte na stránce")
+if (isAdmin) {
+    console.log("přístup do panelu admin povolen"
+)} else {
+    console.log("přístup jako uživatel povolen!")
+}
+}
+// Operátory
+
+let age = 20;
+let hasDriverLicense = true;
+
+if (age >= 18 && hasDriverLicense) {
+    console.log("můžete řídit auto")
+} else {
+    console.log("nemůžete řídit auto")
+}
